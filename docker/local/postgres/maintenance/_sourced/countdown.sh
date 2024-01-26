2b342b4619ebb5c330be14b8f1e519a9d423835b
Paul Onyekwelu <32406844+PaulOnyekwelu@users.noreply.github.com>
2024-01-26 12:05:04 +0000

    Update countdown.sh

--- docker/local/postgres/maintenance/_sourced/countdown.sh
+++ docker/local/postgres/maintenance/_sourced/countdown.sh
@@ -3,7 +3,7 @@
 countdown(){
   declare desc="A simple countdown"
   local seconds="${1}"
-  local d=$(($(date +%s) + "${second}"))
+  local d=$(($(date +%s) + "${seconds}"))
 
   while [ "$d" -ge `date +%s` ]; do
     echo -ne "$(date -u --date @$(($d - `date +%s`)) +%H:%M:%S)\r"

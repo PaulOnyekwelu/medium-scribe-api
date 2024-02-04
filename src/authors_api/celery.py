0f3216db5651d8d369dca0171b25b7baf4c26e6d
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-02-04 16:36:34 +0000

    setup celery, redis, flower

---
+++ src/authors_api/celery.py
@@ -0,0 +1,14 @@
+import os
+from django.conf import settings
+from celery import Celery
+from .settings.base import env
+
+
+# TODO: change in production
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "authors_api.settings.local")
+
+app = Celery(env("APP_TITLE", default="example-api"))
+
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
--- src/authors_api/settings/local.py
+++ src/authors_api/settings/local.py
@@ -14,3 +14,11 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "0.0.0.0"]
 
 CSRF_TRUSTED_ORIGIN = ["http://localhost:8080"]
+
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@paulonyekwelu.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = env("APP_NAME")

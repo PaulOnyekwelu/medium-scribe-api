b8a5aa4d8b39799bbbb5ce0e6f19d34569dedc8e
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2024-02-07 09:52:18 +0000

    code refactoring

--- src/core/users/urls.py
+++ src/core/users/urls.py
@@ -4,9 +4,12 @@ from .views import CustomUserDetailsView
 
 
 urlpatterns = [
+    # it is important to keep the user/ route or any other custom route
+    # at the top of the list. This is because some of the third party packages
+    # may have the same route which will take precedence if kept on the top
+    path("user/", CustomUserDetailsView.as_view(), name="user_details"),
     path("", include("dj_rest_auth.urls")),
     path("registration/", include("dj_rest_auth.registration.urls")),
-    path("user/", CustomUserDetailsView.as_view(), name="user_details"),
     path(
         "password/reset/confirm/<uidb64>/<token>/",
         PasswordResetConfirmView.as_view(),
--- src/core/users/views.py
+++ src/core/users/views.py
@@ -5,7 +5,7 @@ from .serializers import UserSerializer
 
 
 class CustomUserDetailsView(RetrieveUpdateAPIView):
-    serializer_class = UserSerializer()
+    serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
     def get_object(self):
